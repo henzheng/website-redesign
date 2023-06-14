251b67154d6e353d9565483532ecc824e664d9eb
Henry Zheng <zhenghenry2@gmail.com>
2023-06-13 20:08:28 -0400

    adding content to experience

--- pages/experience.vue
+++ pages/experience.vue
@@ -7,5 +7,28 @@
             </div>
             <img class="w-1/2" src="/mining.png" alt="mining">
         </div>
+        <div class="flex flex-col justify-between items-center gap-4">
+            <p class="text-sm font-poppins leading-7 mt-2">
+                Over the course of my high school career, I have always been proactive in seeking out internship opportunities. 
+                I've been fortunate enough to have the opportunity to work for various tech companies, where <i>I honed my programming abilities and learned to adapt to a professional environment.</i> 
+                These internships have provided me with invaluable insights into the tech industry, and allowed me to work alongside experienced professionals.
+                Through these experiences, I have grown both personally and professionally, paving the way for a promising future in the field of technology.
+                For a more comprehensive overview, take a look at my resume!
+            </p>
+            <!-- Create Tabs Here -->
+            <div class="flex flex-row items-center w-full">
+                <div class="flex flex-col items-center justify-center">
+
+                </div>
+                <div class="flex flex-col items-center justify-center">
+                    
+                </div>
+
+            </div>
+        </div>
     </div>
 </template>
+
+<script setup>
+
+</script>
