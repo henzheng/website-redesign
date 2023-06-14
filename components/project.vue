bd29ddc59da55973d9d81b07882ef583182b09b4
Henry Zheng <zhenghenry2@gmail.com>
2023-06-14 16:53:44 -0400

    modified index projects page working

---
+++ components/project.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex flex-row gap-4">
+        <div class="flex items-center justify-center aspect-video w-1/2">
+            <a :href="url">
+                <img class="rounded-md" :src="imagePath" :alt="projectName">    
+            </a>
+        </div>
+        <div class="flex flex-col w-1/2">
+            <h3 class="font-poppins text-lg">{{ projectName }}</h3>
+            <p class="font-poppins">{{ projectRole }}</p>
+            <div class="bg-slate-800 opacity-80 rounded-sm text-white p-3 my-4">
+                <p class="text-white font-poppins text-sm">{{ description }}</p>
+            </div>
+            <ul class="flex flex-row gap-4">
+                <li class="font-poppins" v-for="(tech, index) in technologies" :id="index.toString()">{{ tech }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    url: String,
+    imagePath: String,
+    projectName: String,
+    projectRole: String,
+    description: String,
+    technologies: Array
+})
+</script>
--- pages/projects.vue
+++ pages/projects.vue
@@ -2,10 +2,56 @@
     <div class="pt-[6.5rem] px-0 mt-0 mx-[2rem] w-full">
         <div class="flex flex-col items-center justify-center">
             <div class="flex flex-row w-full items-center pb-4">
-                <h1 class="font-poppins text-[2rem] mr-4">03. My Projects </h1>
+                <h1 class="font-poppins text-[2rem] mr-4">03. Featured Projects </h1>
                 <hr class="border-[1.5px] border-black w-[200px]">
             </div>
-            <img class="w-1/2" src="/images/picnic.png" alt="picnic">
+            <!-- <img class="w-1/2" src="/images/picnic.png" alt="picnic"> -->
+        </div>
+        <div class="flex flex-col gap-[5rem] mb-5">
+            <Project v-for="project in data" 
+                class="mt-5 swap"
+                :url="project.url" 
+                :image-path="project.imagePath" 
+                :project-role="project.projectRole" 
+                :description="project.description" 
+                :technologies="project.technologies" 
+                :project-name="project.projectName"
+            />
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+const data = [
+    {
+        url: "/",
+        imagePath: "/images/sinnoh.jpg",
+        projectName: "Portfolio Website",
+        projectRole: "Sole Developer",
+        description: "A simple, clean portfolio website displaying my information and accomplishments.",
+        technologies: ["Nuxt.js", "TailwindCSS"]
+    },
+    {
+        url: "https://github.com/nullZheng/TravelerJS",
+        imagePath: "/images/genshin.jpg",
+        projectName: "Traveler Discord Bot",
+        projectRole: "Sole Developer",
+        description: "An easy-to-use Genshin Impact wiki right in the comfort of your Discord server!",
+        technologies: ["Javascript", "Discord.js"]
+    },
+    {
+        url: "https://github.com/nullZheng/Data-Visualization",
+        imagePath: "/images/twitter.jpg",
+        projectName: "Pokemon Data Visualization",
+        projectRole: "Sole Developer",
+        description: "Take a look at the data behind the with original 151 Pokemon with stunning visuals and a new perspective.",
+        technologies: ["Svelte", "D3.js", "Chart.js"]
+    }
+]
+</script>
+
+<style scoped>
+.swap:nth-of-type(even) {
+    flex-direction: row-reverse;
+}
+</style>
