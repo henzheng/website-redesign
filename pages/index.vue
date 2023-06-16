3fa90def0d8e27e5f393ae5684886ee7b58eb59f
Henry Zheng <zhenghenry2@gmail.com>
2023-06-16 12:23:31 -0400

    update text-content

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@
                 If you couldn't already tell, I'm a big fan of Pokemon and it served as the inspiration behind this website.
                 Pokemon Diamond and Pearl is my favorite game from the series, and it was the game the introduced me to the world of Pokemon.
                 From the very beginning, the game's seemingly endless world, beautiful music, and intriguing characters capitivated my imagination and fueled my love for the franchise.
-                From trading and collecting Pokemon cards during my childhood to delving into the competitive scene of Sword and Shield, where I proudly attained master ball rank in both singles and doubles battle formats, my journey as a part-time Pokemon trainer has been exhilarating. 
+                From trading and collecting Pokemon cards during my childhood to delving into the competitive scene of Sword and Shield, where I proudly attained master ball rank in both singles and doubles battle formats, my journey as a part-time Pokemon trainer and developer has been exhilarating. 
             </p>
         </div>
     </div>
