7582cb6174a81387c1891b025a82a305cc1bb9ed
Henry Zheng <zhenghenry2@gmail.com>
2023-06-15 22:34:53 -0400

    slight margin adjustments

--- pages/about.vue
+++ pages/about.vue
@@ -18,14 +18,14 @@
                     In fact, this website was built using <i>Nuxt.js and Tailwind CSS!</i>
                 </p>
                 <div class="flex flex-row gap-[2.75rem] mt-2 mb-4">
-                    <p class="text-sm font-poppins leading-7 mt-2 mb-4">
+                    <p class="text-sm font-poppins leading-7">
                         Growing up in the vibrant metropolis of New York City, I've always lived by the motto "Excelsior," meaning "ever upward." 
                         This motto resonates deeply with my approach to programming. 
                         I firmly believe in the power of constant improvement and never settling for mediocrity. 
                         With each project, I strive to push boundaries, embrace innovation, and deliver exceptional web experiences.
                         I'm planning to major in Computer Science in the near future to pursue my dreams of becoming a software engineer.
                     </p>
-                    <img class="w-1/3 aspect-auto rounded mr-5" src="/images/pbw-promo-artwork.jpg" alt="childhood">
+                    <img class="w-1/3 aspect-auto rounded mr-8" src="/images/pbw-promo-artwork.jpg" alt="childhood">
                 </div>
             </div>
         </div>
