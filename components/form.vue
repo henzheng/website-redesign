4cf38447c6b8a7101a5d2ee86869e8164cd77e2d
Henry Zheng <zhenghenry2@gmail.com>
2023-11-13 01:19:27 -0500

    working google form

--- components/form.vue
+++ components/form.vue
@@ -1,33 +1,27 @@
 <template>
-    <div class="flex flex-col justify-center items-center w-full h-3/4 md:w-[350px] md:h-[350px]
+    <div class="flex flex-col justify-center items-center w-full h-[350px] md:w-[350px]
                 rounded shadow-2xl bg-white dark:bg-slate-800">
-        <h3 class="font-poppins text-xl mt-10 mb-4">Send me a message!</h3>
-        <form @submit.prevent="sendEmail" autocomplete="off" class="font-poppins flex flex-col text-sm gap-4 w-3/4 h-3/4">
-            <input 
+        <h3 class="font-poppins text-lg md:text-xl mt-10 mb-4">Send me a message!</h3>
+        <form
+              action="https://docs.google.com/forms/d/e/1FAIpQLSd4DhlVsmVRcgd0sKzOdg5ofAQcCwhv_ZBgn_wvE7tObK51GQ/formResponse"
+              method="post" target="_blank"
+              autocomplete="off" class="font-poppins flex flex-col text-sm gap-4 w-3/4 h-3/4">
+            <input
+                name="entry.268733639"
                 class="outline-none p-2 rounded-lg bg-slate-100 dark:bg-slate-600" 
-                required placeholder="Your name" type="text" id="name">
-            <input 
+                required placeholder="Your name" type="text">
+            <input
+                name="entry.810499223" 
                 class="outline-none p-2 rounded-lg bg-slate-100 dark:bg-slate-600" 
-                required placeholder="Your email" type="email" id="email">
-            <textarea 
-                class="block outline-none p-2 rounded-lg resize-none bg-slate-100 dark:bg-slate-600 text-left"
-                required placeholder="Your message" name="message" id="message" rows="3" cols="30">
-            </textarea>
-            <button class="bg-blue-500 w-[5rem] text-white rounded p-2 mx-auto mb-8" type="submit">
+                required placeholder="Your email" type="email">
+            <input
+                name="entry.2004209441"
+                class="block outline-none p-2 rounded-lg bg-slate-100 dark:bg-slate-600"
+                required placeholder="Your message" type="text">
+            <button class="bg-blue-500 w-[5rem] text-white rounded p-2 my-auto mx-auto" type="submit">
                 <Icon name="ion:ios-paper-plane" />
                 Send
             </button>
         </form>
     </div>
 </template>
-
-<script setup>
-const email = reactive({
-    name: String,
-    content: String,
-})
-
-async function sendEmail() {
-    console.log("Email button clicked")
-}
-</script>
