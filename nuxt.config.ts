f1c9855f081cf23447a1bf69845f1b910a63e655
Henry Zheng <73442345+henzheng@users.noreply.github.com>
2024-11-20 17:59:08 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'netlify',
+  },
   css: ['~/assets/css/main.css'],
   modules: [
       '@nuxtjs/tailwindcss',
